b00886ee6e0820c5f59325eae34d05c0153abb10
BIGESTDS\fujin <fujintaran3@gmail.com>
2023-06-27 14:51:29 +0900

    bowse

--- Assets/Scripts/BowFolder/BowSE.cs
+++ Assets/Scripts/BowFolder/BowSE.cs
@@ -12,13 +12,27 @@ public class BowSE : MonoBehaviour
 
     #region variable 
 
-    [SerializeField] AudioClip _drawClip = default;
+    [SerializeField] AudioClip[] _drawingClips = default;
+
+    [SerializeField] AudioClip _drawStartClips = default;
 
     [SerializeField] AudioClip _attractClip = default;
 
     [SerializeField] AudioClip _shotClip = default;
 
+    [SerializeField] float _drawSE_MaxWaitTime = 0.7f;
+
+    const int MAX_PERCENT = 1;
+
+    const int SE_INDEX_DRAW = 0;
+
+    const int SE_INDEX_ATTRACT = 1;
+
+    const int SE_INDEX_SHOT = 2;
+
     AudioSource _audio = default;
+
+    float[] cacheTimes = default;
 
     #endregion
     #region property
@@ -31,19 +45,60 @@ public class BowSE : MonoBehaviour
     }
 
 
-    public void CallDrawSE(float drawPower)
+    public void CallDrawingSE(float drawPowerPercent)
     {
-        _audio.PlayOneShot(_drawClip,drawPower);
+        if (LoopSEControlToPercent(drawPowerPercent,_drawSE_MaxWaitTime, SE_INDEX_DRAW))
+        {
+            _audio.PlayOneShot(_drawingClips[Random.Range(0, _drawingClips.Length)], drawPowerPercent);
+
+        }
     }
 
-    public void CallAttractSE()
+    public void CallDrawStart()
     {
-        _audio.PlayOneShot(_attractClip);
+        _audio.PlayOneShot(_drawStartClips);
+    }
+
+    public void CallAttractSE(float attractPowerPercent)
+    {
+        if (LoopSEControlToConst(_attractClip.length, SE_INDEX_ATTRACT))
+        {
+            _audio.PlayOneShot(_attractClip,attractPowerPercent);
+
+        }
     }
 
     public void CallShotSE()
     {
         _audio.PlayOneShot(_shotClip);
     }
+
+
+    private bool LoopSEControlToPercent(float loopSpeed,float maxWaitTime, int indexSE)
+    {
+        if (loopSpeed > MAX_PERCENT)
+        {
+            loopSpeed = MAX_PERCENT;
+        }
+
+        if (cacheTimes[indexSE] + (maxWaitTime * (MAX_PERCENT - loopSpeed)) < Time.time)
+        {
+            cacheTimes[indexSE] = Time.time;
+
+            return true;
+        }
+        return false;
+    }
+
+    private bool LoopSEControlToConst(float waitTime, int indexSE)
+    {
+        if (cacheTimes[indexSE] + waitTime < Time.time)
+        {
+            cacheTimes[indexSE] = Time.time;
+            return true;
+        }
+        return false;
+    }
+
     #endregion
 }
