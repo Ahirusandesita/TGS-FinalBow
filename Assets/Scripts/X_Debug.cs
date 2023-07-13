96a4e489e974903a9d22f877dd7d2da50225c183
Ahirusandesita <titiko225@icloud.com>
2023-07-13 12:26:39 +0900

    X_Debug

--- Assets/Scripts/X_Debug.cs
+++ Assets/Scripts/X_Debug.cs
@@ -2,7 +2,7 @@
 // X_Debug.cs 
 // 
 // CreateDay: 2023/06/09
-// Creator  : TakayanagiSora
+// Creator  : TakayanagiSora & Nomura
 // --------------------------------------------------------- 
 using UnityEngine;
 
@@ -155,6 +155,122 @@ public static class X_Dobug
     }
 }
 public static class X_Debu8
+{
+    public static void Log(object log)
+    {
+        System.Diagnostics.StackFrame caller = new System.Diagnostics.StackFrame(1);
+        string className = caller.GetMethod().ReflectedType.FullName;
+        string methodName = caller.GetMethod().Name;
+
+        Debug.Log("残念ハズレでーすほげぇ（　’＿　’）");
+    }
+
+    public static void LogWarning(object log)
+    {
+        System.Diagnostics.StackFrame caller = new System.Diagnostics.StackFrame(1);
+        string className = caller.GetMethod().ReflectedType.FullName;
+        string methodName = caller.GetMethod().Name;
+
+        Debug.LogWarning("残念ハズレでーすほげぇ（　’＿　’）");
+    }
+
+    public static void LogError(object log)
+    {
+        System.Diagnostics.StackFrame caller = new System.Diagnostics.StackFrame(1);
+        string className = caller.GetMethod().ReflectedType.FullName;
+        string methodName = caller.GetMethod().Name;
+
+        Debug.LogError("残念ハズレでーすほげぇ（　’＿　’）");
+    }
+}
+public static class XDebug
+{
+    public static void Log(object log)
+    {
+        System.Diagnostics.StackFrame caller = new System.Diagnostics.StackFrame(1);
+        string className = caller.GetMethod().ReflectedType.FullName;
+        string methodName = caller.GetMethod().Name;
+
+        Debug.Log("残念ハズレでーすほげぇ（　’＿　’）");
+    }
+
+    public static void LogWarning(object log)
+    {
+        System.Diagnostics.StackFrame caller = new System.Diagnostics.StackFrame(1);
+        string className = caller.GetMethod().ReflectedType.FullName;
+        string methodName = caller.GetMethod().Name;
+
+        Debug.LogWarning("残念ハズレでーすほげぇ（　’＿　’）");
+    }
+
+    public static void LogError(object log)
+    {
+        System.Diagnostics.StackFrame caller = new System.Diagnostics.StackFrame(1);
+        string className = caller.GetMethod().ReflectedType.FullName;
+        string methodName = caller.GetMethod().Name;
+
+        Debug.LogError("残念ハズレでーすほげぇ（　’＿　’）");
+    }
+}
+public static class X_Dedug
+{
+    public static void Log(object log)
+    {
+        System.Diagnostics.StackFrame caller = new System.Diagnostics.StackFrame(1);
+        string className = caller.GetMethod().ReflectedType.FullName;
+        string methodName = caller.GetMethod().Name;
+
+        Debug.Log("残念ハズレでーすほげぇ（　’＿　’）");
+    }
+
+    public static void LogWarning(object log)
+    {
+        System.Diagnostics.StackFrame caller = new System.Diagnostics.StackFrame(1);
+        string className = caller.GetMethod().ReflectedType.FullName;
+        string methodName = caller.GetMethod().Name;
+
+        Debug.LogWarning("残念ハズレでーすほげぇ（　’＿　’）");
+    }
+
+    public static void LogError(object log)
+    {
+        System.Diagnostics.StackFrame caller = new System.Diagnostics.StackFrame(1);
+        string className = caller.GetMethod().ReflectedType.FullName;
+        string methodName = caller.GetMethod().Name;
+
+        Debug.LogError("残念ハズレでーすほげぇ（　’＿　’）");
+    }
+}
+public static class X_DobugLog
+{
+    public static void Log(object log)
+    {
+        System.Diagnostics.StackFrame caller = new System.Diagnostics.StackFrame(1);
+        string className = caller.GetMethod().ReflectedType.FullName;
+        string methodName = caller.GetMethod().Name;
+
+        Debug.Log("残念ハズレでーすほげぇ（　’＿　’）");
+    }
+
+    public static void LogWarning(object log)
+    {
+        System.Diagnostics.StackFrame caller = new System.Diagnostics.StackFrame(1);
+        string className = caller.GetMethod().ReflectedType.FullName;
+        string methodName = caller.GetMethod().Name;
+
+        Debug.LogWarning("残念ハズレでーすほげぇ（　’＿　’）");
+    }
+
+    public static void LogError(object log)
+    {
+        System.Diagnostics.StackFrame caller = new System.Diagnostics.StackFrame(1);
+        string className = caller.GetMethod().ReflectedType.FullName;
+        string methodName = caller.GetMethod().Name;
+
+        Debug.LogError("残念ハズレでーすほげぇ（　’＿　’）");
+    }
+}
+public static class X_Debug<T>
 {
     public static void Log(object log)
     {
