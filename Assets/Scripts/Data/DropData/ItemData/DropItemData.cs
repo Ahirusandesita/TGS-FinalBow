784e4232cd3684880ef8e0ba2fbf2bb3da6996b3
Ahirusan <titiko225@icloud.com>
2023-08-13 22:11:48 +0900

    DropItem

---
+++ Assets/Scripts/Data/DropData/ItemData/DropItemData.cs
@@ -0,0 +1,14 @@
+// --------------------------------------------------------- 
+// DropItemData.cs 
+// 
+// CreateDay: 
+// Creator  : 
+// --------------------------------------------------------- 
+using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "DropItemData", menuName = "Scriptables/CreateDropItemTable")]
+public class DropItemData : ScriptableObject
+{
+    public int DropAngle;
+}
---
+++ Assets/Scripts/NewSystem/DropItem.cs
@@ -0,0 +1,32 @@
+// --------------------------------------------------------- 
+// DropItem.cs 
+// 
+// CreateDay: 
+// Creator  : 
+// --------------------------------------------------------- 
+using UnityEngine;
+using System.Collections;
+public class DropItem : MonoBehaviour
+{
+ #region variable 
+ #endregion
+ #region property
+ #endregion
+ #region method
+ 
+ private void Awake()
+ {
+
+ }
+ 
+ private void Start ()
+ {
+
+ }
+
+ private void Update ()
+ {
+
+ }
+ #endregion
+}
