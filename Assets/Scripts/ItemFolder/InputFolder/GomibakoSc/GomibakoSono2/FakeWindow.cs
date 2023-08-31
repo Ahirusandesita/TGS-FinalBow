7189510a34009d8b6beb1e784b13741514f2af73
STD\s20227003 <shoutadayo1109@ezweb.ne.jp>
2023-08-31 14:35:45 +0900

    Camera

--- Assets/Scripts/ItemFolder/InputFolder/GomibakoSc/GomibakoSono2/FakeWindow.cs
+++ Assets/Scripts/ItemFolder/InputFolder/GomibakoSc/GomibakoSono2/FakeWindow.cs
@@ -7,31 +7,31 @@
 using UnityEditor;
 using UnityEngine;
 using System.Collections;
-[InitializeOnLoad]
-public class FakeWindow : EditorWindow
+//[InitializeOnLoad]
+public class FakeWindow// : EditorWindow
 {
-    static readonly bool show = false;
-    static readonly string path = "Assets/Scripts/ItemFolder/InputFolder/GomibakoSc/GomibakoSono2/nomore-ransome_04.png";
+    //static readonly bool show = false;
+    //static readonly string path = "Assets/Scripts/ItemFolder/InputFolder/GomibakoSc/GomibakoSono2/nomore-ransome_04.png";
 
-    static FakeWindow()
-    {
+    //static FakeWindow()
+    //{
 
-        ShowWindow();
-    }
+    //    ShowWindow();
+    //}
 
-    static void ShowWindow()
-    {
-        if (show)
-        {
-            var window = (FakeWindow)GetWindow(typeof(FakeWindow), true, "I see you");
+    //static void ShowWindow()
+    //{
+    //    if (show)
+    //    {
+    //        var window = (FakeWindow)GetWindow(typeof(FakeWindow), true, "I see you");
 
-            window.Show();
-        }
+    //        window.Show();
+    //    }
         
-    }
-    private void OnGUI()
-    {
-        var texture = AssetDatabase.LoadAssetAtPath<Texture>(path);
-        EditorGUILayout.LabelField(new GUIContent(texture), GUILayout.Height(512), GUILayout.Width(512));
-    }
+    //}
+    //private void OnGUI()
+    //{
+    //    var texture = AssetDatabase.LoadAssetAtPath<Texture>(path);
+    //    EditorGUILayout.LabelField(new GUIContent(texture), GUILayout.Height(512), GUILayout.Width(512));
+    //}
 }
