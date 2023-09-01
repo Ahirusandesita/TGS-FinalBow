5a29ee11f20055315b60ee2d241aded2b953b817
STD\s20227064 <20227064-sasakiisssei@hcs.ac.jp>
2023-09-01 10:04:45 +0900

    gimmickごりおし

--- Assets/Scripts/ItemFolder/InputFolder/GimmickFolder/GimmickDropItem.cs
+++ Assets/Scripts/ItemFolder/InputFolder/GimmickFolder/GimmickDropItem.cs
@@ -6,7 +6,25 @@
 // --------------------------------------------------------- 
 using UnityEngine;
 using System.Collections;
-public class GimmickDropItem : MonoBehaviour,
+public class GimmickDropItem : MonoBehaviour, IFGimmickCaller
 {
- 
+    [SerializeField] GameObject breakObject;
+
+    [SerializeField] Drop drop;
+
+    [SerializeField] DropData data;
+    public bool IsFinish => used;
+
+    public bool Moving => used;
+
+    bool used = false;
+
+    public void GimmickAction()
+    {
+        drop.DropStart(data, breakObject.transform.position);
+
+        breakObject.SetActive(false);
+
+        used = true;
+    }
 }
