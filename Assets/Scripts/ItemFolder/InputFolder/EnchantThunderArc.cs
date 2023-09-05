4590bcf76302f9f5eb3e8758b88b337be6205eac
STD\s20227064 <20227064-sasakiisssei@hcs.ac.jp>
2023-09-05 19:55:15 +0900

    enc

---
+++ Assets/Scripts/ItemFolder/InputFolder/EnchantThunderArc.cs
@@ -0,0 +1,24 @@
+// --------------------------------------------------------- 
+// EnchantThunderArc.cs 
+// 
+// CreateDay: 
+// Creator  : 
+// --------------------------------------------------------- 
+using UnityEngine;
+using System.Collections;
+public class EnchantThunderArc
+{
+ #region variable 
+
+
+ #endregion
+ #region property
+ #endregion
+ #region method
+ 
+    private void FindObject()
+    {
+
+    }
+ #endregion
+}
