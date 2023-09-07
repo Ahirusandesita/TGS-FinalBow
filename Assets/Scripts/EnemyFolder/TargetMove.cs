2c12b3b6b0f720f7bf061e51d01584641e151ee5
STD\s20227082 <20227082-takayanagisora@hcs.ac.jp>
2023-09-07 19:58:24 +0900

    追加 >> 的の移動クラス

---
+++ Assets/Scripts/EnemyFolder/TargetMove.cs
@@ -0,0 +1,40 @@
+// --------------------------------------------------------- 
+// TargetMove.cs 
+// 
+// CreateDay: 
+// Creator  : 
+// --------------------------------------------------------- 
+using UnityEngine;
+using System.Collections;
+public class TargetMove : MonoBehaviour
+{
+    #region variable 
+    #endregion
+    #region property
+    #endregion
+    #region method
+
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// ‰ñ“]‚µ‚ÄƒXƒ|[ƒ“‚·‚éˆ—
+    /// </summary>
+    private void SpawnRotate()
+    {
+
+    }
+    #endregion
+}
