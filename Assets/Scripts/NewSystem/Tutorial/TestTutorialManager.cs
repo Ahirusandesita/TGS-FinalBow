f1630d329acc0eb55c9e6f826201670e9ca61107
Ahirusan <titiko225@icloud.com>
2023-09-09 14:34:28 +0900

    Tutorial進行

--- Assets/Scripts/NewSystem/Tutorial/TestTutorialManager.cs
+++ Assets/Scripts/NewSystem/Tutorial/TestTutorialManager.cs
@@ -9,15 +9,24 @@ using System.Collections;
 
 public enum TutorialType
 {
+    start,
     enchant,
     attarct,
     shot,
     shotComesOff,
-    crystalBreak
+    crystalBreak,
+    end
 }
 
 public class TestTutorialManager : MonoBehaviour
 {
     public IReActiveProperty<TutorialType> raedOnlyTutorial => TutorialProperty;
     private ReActiveProperty<TutorialType> TutorialProperty = new ReActiveProperty<TutorialType>();
+
+    public void StartEnchant() => TutorialProperty.Value = TutorialType.enchant;//ボムしか選べなくする
+    public void EnchantEnd() => TutorialProperty.Value = TutorialType.attarct;//吸い込もう
+    public void AttractEnd() => TutorialProperty.Value = TutorialType.shot; //撃とう見たいなテキストだすイベント
+    public void ShotComesOff() => TutorialProperty.Value = TutorialType.shot;//もう一回撃とう
+    public void ShotEnd() => TutorialProperty.Value = TutorialType.crystalBreak;//crystalブレイク開始
+    public void CryStalBreakEnd() => TutorialProperty.Value = TutorialType.end;
 }
