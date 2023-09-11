0073eae1f4fe11d3fbe928fa55b3d937513d8744
STD\s20227082 <20227082-takayanagisora@hcs.ac.jp>
2023-09-11 16:00:59 +0900

    変更 >> ワームのデータ構造

--- Assets/Scripts/Data/EnemyData/GroundEnemyDataTable.cs
+++ Assets/Scripts/Data/EnemyData/GroundEnemyDataTable.cs
@@ -19,23 +19,17 @@ public enum WormType
 [CreateAssetMenu(fileName = "GroundEnemyData", menuName = "Scriptables/CreateGroundEnemyDataTable")]
 public class GroundEnemyDataTable : ScriptableObject
 {
-    [Header("スポーンディレイ（注：ステージ開始時からの時間）"), Tooltip("スポーンディレイ（注：ステージ開始時からの時間）")]
-    public float _spawnDelay_s;
-
-    [Header("地上雑魚のスポーン位置"), Tooltip("地上雑魚のスポーン位置")]
+    [Header("地上雑魚/砂埃のスポーン位置"), Tooltip("地上雑魚/砂埃のスポーン位置")]
     public Transform _groundEnemySpawnPlace;
 
-    [Header("砂埃のスポーン位置"), Tooltip("砂埃のスポーン位置")]
-    public Transform _cloudOfDustPlace;
+    [Header("出現する時刻"), Tooltip("出現する時刻")]
+    public float _spawnTime_s;
 
-    [Header("出現間隔"), Tooltip("出現間隔")]
-    public float _appearanceInterval_s;
+    [Header("砂埃のみ出現"), Tooltip("砂埃のみ出現")]
+    public bool _onlySandDust;
 
-    [Header("出現時間"), Tooltip("出現時間")]
+    [Header("出現キープ時間"), Tooltip("出現キープ時間")]
     public float _appearanceKeep_s;
-
-    [Header("デスポーンまでの秒数"), Tooltip("デスポーンまでの秒数")]
-    public float _despawnTime_s = 10f;
 
     [Header("出現の仕方"), Tooltip("出現の仕方")]
     public WormType _wormType;
