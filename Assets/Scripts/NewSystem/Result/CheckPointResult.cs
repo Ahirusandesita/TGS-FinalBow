57402c296de990abb6925c17f5081ccf82658c94
STD\s20227003 <shoutadayo1109@ezweb.ne.jp>
2023-09-12 15:35:15 +0900

    mozituika

--- Assets/Scripts/NewSystem/Result/CheckPointResult.cs
+++ Assets/Scripts/NewSystem/Result/CheckPointResult.cs
@@ -7,10 +7,19 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
+
 public class CheckPointResult : MonoBehaviour
 {
     #region variable 
-    private Text text;
+    public TextMeshProUGUI killCountText;
+    public TextMeshProUGUI killScoreText;
+    public TextMeshProUGUI numberOfCombosText;
+    public TextMeshProUGUI clearTimeText;
+    public TextMeshProUGUI sumScoreText;
+
+    public List<TMP_FontAsset> fontAssets = new List<TMP_FontAsset>();
 
     public struct ResultStruct
     {
@@ -27,13 +36,32 @@ public class CheckPointResult : MonoBehaviour
 
     private void Awake()
     {
-        text = this.GetComponent<Text>();
+        //text = this.GetComponent<TextMeshProUGUI>();
     }
 
     public void Result(ResultStruct resultStruct)
     {
-        string killsToString = resultStruct.NumberOfKills.ToString();
-        text.text = "撃破数" + killsToString + "   撃破スコア" + resultStruct.KillsScore + "\n" + "コンボスコア" + resultStruct.NumberOfCombos.ToString() + "\n" + "クリアタイム" + resultStruct.ClearTime.ToString() + "\n" + "合計スコア" + resultStruct.SumScore.ToString();
+        killCountText.text = "撃破数" + resultStruct.NumberOfKills;
+        killScoreText.text = "撃破スコア" + resultStruct.KillsScore;
+        numberOfCombosText.text = "コンボスコア" + resultStruct.NumberOfCombos;
+        clearTimeText.text = "クリアタイム" + resultStruct.ClearTime.ToString();
+        sumScoreText.text = "合計スコア" + resultStruct.SumScore.ToString();
+
+    }
+    private void Update()
+    {
+        int nowint = default;
+        int lastInt = default;
+        while (nowint == lastInt)
+            nowint = Random.Range(0, fontAssets.Count - 1);
+
+        lastInt = nowint;
+
+        killCountText.font = fontAssets[nowint];
+        killScoreText.font = fontAssets[nowint];
+        numberOfCombosText.font = fontAssets[nowint];
+        clearTimeText.font = fontAssets[nowint];
+        sumScoreText.font = fontAssets[nowint];
     }
     #endregion
 }
