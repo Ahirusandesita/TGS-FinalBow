3ab7959626a09c408490765b70d73904ac9c1ac6
STD\s20227064 <20227064-sasakiisssei@hcs.ac.jp>
2023-09-12 17:10:41 +0900

    re-za-

--- Assets/Scripts/ItemFolder/InputFolder/TitleRazerManager.cs
+++ Assets/Scripts/ItemFolder/InputFolder/TitleRazerManager.cs
@@ -6,27 +6,111 @@
 // --------------------------------------------------------- 
 using UnityEngine;
 using System.Collections;
+using System;
+
+interface IFTakeRayCastTitle
+{
+    void TakeRay();
+
+    void SelectingRay();
+
+    void SelectedRay();
+}
 public class TitleRazerManager : MonoBehaviour
 {
- #region variable 
- #endregion
- #region property
- #endregion
- #region method
- 
- private void Awake()
- {
+    [SerializeField] Transform leftRazerStartPosition = default;
+    [SerializeField] Transform rightRazerStartPosition = default;
 
- }
- 
- private void Start ()
- {
+    [SerializeField] Transform leftPointer = default;
+    [SerializeField] Transform rightPointer = default;
 
- }
+    [SerializeField] LineRenderer leftRenderer = default;
+    [SerializeField] LineRenderer rightRenderer = default;
 
- private void Update ()
- {
+    [SerializeField] float maxLineLength = 100f;
 
- }
- #endregion
+    InputManagement input = default;
+
+    DirectionParts left;
+    DirectionParts right;
+
+    const int START_INDEX = 0;
+
+    struct DirectionParts
+    {
+        public Transform start;
+        public Transform pointer;
+        public LineRenderer renderer;
+        public RaycastHit hit;
+        public Func<bool>buttonPushing;
+        public IFTakeRayCastTitle cache;
+        public bool selecting;
+        public DirectionParts(Transform startPosition,Transform pointer,LineRenderer renderer,Func<bool>input)
+        {
+            this.start = startPosition;
+            this.pointer = pointer;
+            this.renderer = renderer;
+            hit = default;
+            buttonPushing = input;
+            cache = default;
+            selecting = false;
+        }
+
+        public Vector3 GetStartPosition()
+        {
+            return start.position;
+        }
+
+        public Vector3 GetPointPosition()
+        {
+            return pointer.position;
+        }
+    }
+
+    private void Awake()
+    {
+        input = GetComponent<InputManagement>();
+        leftRenderer.SetPosition(START_INDEX, left.GetStartPosition());
+        rightRenderer.SetPosition(START_INDEX, right.GetStartPosition());
+
+        left = new DirectionParts(leftRazerStartPosition, leftPointer, leftRenderer,input.ButtonDownLeftUpTrigger);
+        right = new DirectionParts(rightRazerStartPosition, rightPointer, rightRenderer,input.ButtonRightUpTrigger);
+    }
+    private void Update()
+    {
+        RayFindObject(left);
+        RayFindObject(right);
+
+        SetRayPoint(left);
+        SetRayPoint(right);
+
+        
+
+    }
+
+    private void RayFindObject(DirectionParts parts)
+    {
+        if (Physics.Raycast(parts.GetStartPosition(), parts.start.forward, out parts.hit, maxLineLength))
+        {
+            parts.cache = parts.hit.collider.GetComponent<IFTakeRayCastTitle>();
+            parts.cache.TakeRay();
+
+            // ‚±‚±‚ÉƒŒƒtƒg
+
+        }
+        else
+        {
+            parts.cache = default;
+        }
+    }
+
+    private void SetRayPoint(DirectionParts parts)
+    {
+        const int END_INDEX = 1;
+        parts.renderer.SetPosition(END_INDEX, parts.hit.point);
+        parts.pointer.position = parts.hit.point;
+    }
+    
+       
+    
 }
