4905fa9c98540d3b268d117b49e503ec71286df3
STD\s20227002 <shironeko248@gmail.com>
2023-09-13 17:56:09 +0900

    ちょくちょく調整

---
+++ Assets/Personal/NishigakiFolder/StringCreater.cs
@@ -0,0 +1,32 @@
+// --------------------------------------------------------- 
+// StringCreater.cs 
+// 
+// CreateDay: 
+// Creator  : 
+// --------------------------------------------------------- 
+using UnityEngine;
+using System.Collections;
+public class StringCreater : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _stringOver = default;
+    [SerializeField]
+    private Transform _stringCenter = default;
+    [SerializeField]
+    private Transform _stringUnder = default;
+
+    private void Awake()
+    {
+
+    }
+ 
+    private void Start ()
+    {
+
+    }
+
+    private void Update ()
+    {
+
+    }
+}
