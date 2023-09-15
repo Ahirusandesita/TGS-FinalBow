d3d03dd7cd2c10bd77a252d582bf59b990f33690
STD\s20227064 <fujintaran3@gmail.com>
2023-09-15 12:20:37 +0900

    定期

--- Assets/Scripts/ItemFolder/InputFolder/ParticleCallbackPool.cs
+++ Assets/Scripts/ItemFolder/InputFolder/ParticleCallbackPool.cs
@@ -9,21 +9,38 @@ using System.Collections;
 public class ParticleCallbackPool : MonoBehaviour
 {
     ObjectPoolSystem pool = default;
-    Transform _transform = default;
+    ParticleSystem particle = default;
+    [SerializeField]CashObjectInformation root = default;
+    //Transform _transform = default;
 
-    int particles = 0;
+    //int particles = 0;
     private void Awake()
     {
         pool = FindObjectOfType<ObjectPoolSystem>();
-      
-        foreach(Transform t in _transform)
+        particle = GetComponent<ParticleSystem>();
+        if(root is null)
         {
+            root = transform.GetComponent<CashObjectInformation>();
 
+            if (root is null)
+            {
+                root = transform.parent.GetComponent<CashObjectInformation>();
+            }
         }
+        
+        //foreach(Transform t in _transform)
+        //{
+
+        //}
     }
 
     private void OnEnable()
     {
+        particle.Play(true);
+    }
+    private void OnParticleSystemStopped()
+    {
         
+        pool.ReturnObject(root);
     }
 }
