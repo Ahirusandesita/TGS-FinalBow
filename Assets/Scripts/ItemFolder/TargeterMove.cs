5c4e3e4bc3dca69c0389666f585cdc57ecd11b52
STD\s20227002 <shironeko248@gmail.com>
2023-09-19 18:36:54 +0900

    調整

--- Assets/Scripts/ItemFolder/TargeterMove.cs
+++ Assets/Scripts/ItemFolder/TargeterMove.cs
@@ -50,7 +50,7 @@ public class TargeterMove : MonoBehaviour
         _object = this.gameObject;
 
         // 弓のオブジェクトを取得
-        _player = GameObject.Find("Main Camera").gameObject;
+        _player = GameObject.Find("ArrowLookPoint").gameObject;
     }
 
     private void Update()
